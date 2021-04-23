a4a01bf05d00adc7126af2cff56eb43538ed65b0
Sandra <sandra@okaeri.cloud>
2021-04-23 13:30:12 +0200

    Replace lateinit with constructors, narrow down mapping types

--- api/src/main/java/net/funnyguilds/drama/api/DramaController.kt
+++ api/src/main/java/net/funnyguilds/drama/api/DramaController.kt
@@ -19,30 +19,30 @@ package net.funnyguilds.drama.api
 
 import net.funnyguilds.drama.config.DramaConfig
 import net.funnyguilds.drama.provider.DramaProvider
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/drama")
-class DramaController {
+class DramaController(
+    val dramaProvider: DramaProvider,
+    val dramaConfig: DramaConfig
+) {
 
-    @Autowired private lateinit var dramaProvider: DramaProvider
-    @Autowired private lateinit var dramaConfig: DramaConfig
-
-    @RequestMapping
+    @GetMapping
     fun drama(): ResponseEntity<*> {
         return ResponseEntity.ok(DramaDeclaration(dramaConfig.staticElements, dramaConfig.sentences))
     }
 
-    @RequestMapping("/create")
+    @GetMapping("/create")
     fun dramaCreate(): ResponseEntity<*> {
         return ResponseEntity.ok(DramaResponse(dramaProvider.create()))
     }
 
-    @RequestMapping("/create/{dramas}")
+    @GetMapping("/create/{dramas}")
     fun dramaCreateMultiple(@PathVariable dramas: Int): ResponseEntity<*> {
         return if (dramas < 1 || dramas > 100) {
             ResponseEntity.badRequest().body(DramaError("dramas should be between 1 and 100"))
