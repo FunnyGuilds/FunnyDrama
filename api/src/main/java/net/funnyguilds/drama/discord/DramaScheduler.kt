53b8d0a125976893ede88e305fd1d682e94dc9c0
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-04-23 13:30:12 +0200

    Replace lateinit with constructors, narrow down mapping types

--- api/src/main/java/net/funnyguilds/drama/discord/DramaScheduler.kt
+++ api/src/main/java/net/funnyguilds/drama/discord/DramaScheduler.kt
@@ -18,17 +18,16 @@
 package net.funnyguilds.drama.discord
 
 import net.funnyguilds.drama.config.DiscordConfig
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 @EnableScheduling
-class DramaScheduler {
-
-    @Autowired private lateinit var discordConfig: DiscordConfig
-    @Autowired private lateinit var discordDramaProvider: DiscordDramaProvider
+class DramaScheduler(
+    val discordConfig: DiscordConfig,
+    val discordDramaProvider: DiscordDramaProvider
+) {
 
     @Scheduled(cron = "0 0 0 * * *")
     fun midnightDrama() {
