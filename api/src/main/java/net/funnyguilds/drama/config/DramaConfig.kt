e0903daf7c224b18ae225d3a7a1e4ec9d28a3ef0
Peridot <peridot491@protonmail.com>
2022-04-08 23:24:35 +0200

    Add more minecraft versions

--- api/src/main/java/net/funnyguilds/drama/config/DramaConfig.kt
+++ api/src/main/java/net/funnyguilds/drama/config/DramaConfig.kt
@@ -98,7 +98,7 @@ class DramaConfig : OkaeriConfig() {
             "big", "large", "huge", "gigantic", "enormous"
         ),
         "mcversion" to listOf(
-            "1.6.4", "1.7.2", "1.8.8", "1.9.4", "1.10.2", "1.11.2", "1.12.2", "1.13.2", "1.14.4", "1.15.2", "1.16.5"
+            "1.6.4", "1.7.2", "1.8.8", "1.9.4", "1.10.2", "1.11.2", "1.12.2", "1.13.2", "1.14.4", "1.15.2", "1.16.5", "1.17.1", "1.18.2"
         ),
         "channels" to listOf(
             "#general", "#coding", "#support", "#secret", "#chinskie"
